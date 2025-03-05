1cfd7c5c1034070c2d3b7f14b362e4263d1322d6
Praveen <praveengandikota2222@gmail.com>
2025-03-05 18:58:32 +0530

    Fixed CORS issue in Vercel config

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv"; 
 import { adminRouter } from "./Routes/AdminRoute.js";
-import Jwt from "jsonwebtoken";
+// import Jwt from "jsonwebtoken";
 import cookieParser from "cookie-parser";
 
 dotenv.config();  
 
 const app = express();
 
-app.post("/auth/adminlogin", (req, res) => {
-    res.json({ message: "Login Successful" });
-  });
+// app.post("/auth/adminlogin", (req, res) => {
+//     res.json({ message: "Login Successful" });
+//   });
 
 app.use(cors({
     origin: 'https://employee-management-system-aplication.netlify.app',
@@ -23,28 +23,29 @@ app.use(express.json());
 app.use(cookieParser());
 app.use('/auth', adminRouter);
 app.use(express.static('Public'));
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 
-const verifyUser = (req, res, next) => {
-    const token = req.cookies.token;
-    if (token) {
-        Jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {  
-            if (err) return res.json({ Status: false, Error: "Wrong Token" });
-            req.id = decoded.id;
-            req.role = decoded.role;
-            next();
-        });
-    } else {
-        return res.json({ Status: false, Error: "Not authenticated" });
-    }
-};
+// const verifyUser = (req, res, next) => {
+//     const token = req.cookies.token;
+//     if (token) {
+//         Jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {  
+//             if (err) return res.json({ Status: false, Error: "Wrong Token" });
+//             req.id = decoded.id;
+//             req.role = decoded.role;
+//             next();
+//         });
+//     } else {
+//         return res.json({ Status: false, Error: "Not authenticated" });
+//     }
+// };
 
-app.get('/verify', verifyUser, (req, res) => {
-    return res.json({ Status: true, role: req.role, id: req.id });
-});
+// app.get('/verify', verifyUser, (req, res) => {
+//     return res.json({ Status: true, role: req.role, id: req.id });
+// });
 
-const PORT = process.env.PORT || 3000; 
+//const PORT = process.env.PORT || 3000; 
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(5000, () => {
+    // console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port 5000`);
 });
