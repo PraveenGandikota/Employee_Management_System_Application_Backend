4bd8469f83dffe45fa9e71035e5b1aa027a5f240
Praveen <praveengandikota2222@gmail.com>
2025-03-05 22:01:12 +0530

    Fixed CORS issue in Vercel config

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv"; 
 import { adminRouter } from "./Routes/AdminRoute.js";
-import Jwt from "jsonwebtoken";
+//import Jwt from "jsonwebtoken";
 import cookieParser from "cookie-parser";
 
 dotenv.config();  
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: 'https://employee-management-system-applicatio.netlify.app',
+    origin: 'https://employee-management-system-aplication.netlify.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
@@ -20,23 +20,23 @@ app.use(cookieParser());
 app.use('/auth', adminRouter);
 app.use(express.static('Public'));
 
-const verifyUser = (req, res, next) => {
-    const token = req.cookies.token;
-    if (token) {
-        Jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {  
-            if (err) return res.json({ Status: false, Error: "Wrong Token" });
-            req.id = decoded.id;
-            req.role = decoded.role;
-            next();
-        });
-    } else {
-        return res.json({ Status: false, Error: "Not authenticated" });
-    }
-};
+// const verifyUser = (req, res, next) => {
+//     const token = req.cookies.token;
+//     if (token) {
+//         Jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {  
+//             if (err) return res.json({ Status: false, Error: "Wrong Token" });
+//             req.id = decoded.id;
+//             req.role = decoded.role;
+//             next();
+//         });
+//     } else {
+//         return res.json({ Status: false, Error: "Not authenticated" });
+//     }
+// };
 
-app.get('/verify', verifyUser, (req, res) => {
-    return res.json({ Status: true, role: req.role, id: req.id });
-});
+// app.get('/verify', verifyUser, (req, res) => {
+//     return res.json({ Status: true, role: req.role, id: req.id });
+// });
 
 const PORT = process.env.PORT || 5000; 
 
