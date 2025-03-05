b0f295af245f28128272189bae7f5fb4968731ff
Praveen <praveengandikota2222@gmail.com>
2025-03-05 21:57:57 +0530

    Fixed CORS issue in Vercel config

--- index.js
+++ index.js
@@ -9,12 +9,8 @@ dotenv.config();
 
 const app = express();
 
-app.post("/auth/adminlogin", (req, res) => {
-    res.json({ message: "Login Successful" });
-  });
-
 app.use(cors({
-    origin: 'https://employee-management-system-aplication.netlify.app',
+    origin: 'https://employee-management-system-applicatio.netlify.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
@@ -23,27 +19,26 @@ app.use(express.json());
 app.use(cookieParser());
 app.use('/auth', adminRouter);
 app.use(express.static('Public'));
-// app.use(express.urlencoded({ extended: true }));
 
-// const verifyUser = (req, res, next) => {
-//     const token = req.cookies.token;
-//     if (token) {
-//         Jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {  
-//             if (err) return res.json({ Status: false, Error: "Wrong Token" });
-//             req.id = decoded.id;
-//             req.role = decoded.role;
-//             next();
-//         });
-//     } else {
-//         return res.json({ Status: false, Error: "Not authenticated" });
-//     }
-// };
+const verifyUser = (req, res, next) => {
+    const token = req.cookies.token;
+    if (token) {
+        Jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {  
+            if (err) return res.json({ Status: false, Error: "Wrong Token" });
+            req.id = decoded.id;
+            req.role = decoded.role;
+            next();
+        });
+    } else {
+        return res.json({ Status: false, Error: "Not authenticated" });
+    }
+};
 
 app.get('/verify', verifyUser, (req, res) => {
     return res.json({ Status: true, role: req.role, id: req.id });
 });
 
-const PORT = process.env.PORT || 4000; 
+const PORT = process.env.PORT || 5000; 
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
